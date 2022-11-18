63e3e5d782c6a6238c523c0e2407c9ce45b42087
elena <elena@localhost>
2022-11-18 11:32:29 +0100

    creation puissance 4

---
+++ src/sp4_elziere_dompietrini/SP4_ELZIERE_DOMPIETRINI.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_elziere_dompietrini;
+
+/**
+ *
+ * @author elena
+ */
+public class SP4_ELZIERE_DOMPIETRINI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    Jeton Jeton1 = new Jeton ("jaune");
+    System.out.println(Jeton1);
+    Jeton Jeton2 = new Jeton ("rouge");
+    System.out.println(Jeton2);
+    
+    }
+    
+}
